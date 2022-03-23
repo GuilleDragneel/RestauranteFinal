ae2cd471009a9fc8abffb7df4affecb0359f1e70
labso03 <labso03@labso03-All-Series>
2022-03-23 11:02:56 -0600

    Se creo el html creraregisto aun no queda el metodo para agregar usuarios

--- src/java/Controller/GuardarCrearUsuarioServlet.java
+++ src/java/Controller/GuardarCrearUsuarioServlet.java
@@ -1,8 +1,8 @@
 /*
 Autor= Guillermo Daniel Cruz Ortega
-Fecha creacion= 17/03/2022
-Fecha actualizacion= 17/03/2022
-Descripcion= Servlet guardar y editar del paquete controller
+Fecha creacion= 23/03/2022
+Fecha actualizacion= 23/03/2022
+Descripcion= Servlet de guardar y crear usuario 
  */
 package Controller;
 
@@ -15,18 +15,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class GuardarEditarUsuarioServlet extends HttpServlet {
+public class GuardarCrearUsuarioServlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int idUsuario1 = Integer.parseInt(request.getParameter("idUsuario1"));
         IUsuarioService service = new UsuarioServiceImpl();
         Usuario u = new Usuario();
+        u.setcodigo(request.getParameter("idUsuario1"));
         u.setnombre_usuario(request.getParameter("nombreusu"));
         u.setcontraseña(request.getParameter("contraseña"));
         u.setNombre(request.getParameter("nombre"));
         u.setSexo(request.getParameter("sexo"));
         u.setEdad(Integer.parseInt(request.getParameter("edad")));
-        service.actualizarRegistro(u);
+        service.crearRegisto(u);
         response.sendRedirect("ListarUsuarioServlet");
     }
-
 }
