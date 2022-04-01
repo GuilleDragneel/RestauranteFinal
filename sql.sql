abb930c9e4e84f086aa2eebfc2694a172b11f23d
Stephanie-Jacqueline <92597182+Stephanie-Jacqueline@users.noreply.github.com>
2022-04-01 09:20:38 -0600

    Script de la base de datos del proyecto
    Esté es un archivo para ejecutar.

---
+++ sql.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE rcm;
+\ c crm
+CREATE TABLE usuario (codigo serial primary key, nombre_usuario varchar(20), contraseña varchar(20), nombre varchar(20), sexo varchar(15), edad integer);
